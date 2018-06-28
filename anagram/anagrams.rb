fa9be2ac2acef30eed3fa60ef8c52b3270083153
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-06-28 16:44:11 -0700

    complete anagram.rb

--- anagram/anagrams.rb
+++ anagram/anagrams.rb
@@ -1,4 +1,4 @@
-def first_anagram(string)
+def string_permutation(string)
   return [string] if string.length <=1
   
   arr = string.chars
@@ -15,9 +15,44 @@ def first_anagram(string)
   result
 end
 
-def anagram?(str1, str2)
-  first_anagram(str1).include? str2
+def first_anagram?(str1, str2)
+  string_permutation(str1).include? str2
 end
 
-p anagram?("elvis", "lives")
-p anagram?("gizmo", "sally")
+def second_anagram?(str1, str2)
+  arr1 = str1.chars 
+  arr2 = str2.chars 
+  arr1.each_with_index do |char1, i1|
+    arr2.each_with_index do |char2, i2|
+      if char1 == char2
+        char1 = arr1[i1] = arr2[i2] = nil 
+      end 
+    end
+  end 
+  arr1.compact.join == arr2.compact.join
+end
+
+def third_anagram?(str1, str2)
+  arr1 = str1.chars.sort
+  arr2 = str2.chars.sort
+  arr1 == arr2
+end
+
+def fourth_anagram?(str1, str2)
+  hash = Hash.new(0)
+  arr1 = str1.chars 
+  arr2 = str2.chars 
+  
+  arr1.each do |char|
+    hash[char] += 1
+  end
+  arr2.each do |char|
+    hash[char] -= 1
+  end
+  
+  hash.values.all?{|val| val == 0}
+end
+
+p fourth_anagram?("elvis", "lives")
+p fourth_anagram?("elvis", "livese")
+p fourth_anagram?("gizmoj", "sally")
